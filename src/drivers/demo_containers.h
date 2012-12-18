1ea68ece0e175d77876c9673705f50177ce855f6
Benjamin Andre <bjandre@gmail.com>
2012-12-18 15:09:02 -0800

    misc code cleanup/reorg, turned on all compiler warnings and fixed most issues.

--- src/drivers/demo_containers.h
+++ src/drivers/demo_containers.h
@@ -30,19 +30,7 @@ struct DemoSimulation {
   std::string use_text_output;
   std::string output_time_units;
 
-  void Print(void) const {
-    std::cout << "  -- Simulation parameters :" << std::endl;
-    std::cout << "    description : " << this->description << std::endl;
-    std::cout << "    engine : " << this->engine << std::endl;
-    std::cout << "    engine inputfile : " << this->engine_inputfile << std::endl;
-    std::cout << "    delta t : " << this->delta_t << std::endl;
-    std::cout << "    num times steps : " << this->num_time_steps << std::endl;
-    std::cout << "    initial condition : " << this->initial_condition << std::endl;
-    std::cout << "    text output : " << this->use_text_output << std::endl;
-    std::cout << "    output time units : " << this->output_time_units << std::endl;
-    std::cout << std::endl;
-
-  };
+  void Print(void) const;
 };
 
 
@@ -53,15 +41,7 @@ struct DemoState {
   double temperature;  // [celsius]
   double aqueous_pressure; // [Pa]
 
-  void Print(void) const {
-    std::cout << "  -- State :" << std::endl;
-    std::cout << "    density : " << this->water_density << std::endl;
-    std::cout << "    saturation : " << this->saturation << std::endl;
-    std::cout << "    porosity : " << this->porosity << std::endl;
-    std::cout << "    temperature : " << this->temperature << std::endl;
-    std::cout << "    pressure : " << this->aqueous_pressure << std::endl;
-    std::cout << std::endl;
-  };
+  void Print(void) const;
 };
 
 struct DemoMaterialProperties {
@@ -69,15 +49,7 @@ struct DemoMaterialProperties {
   std::vector<double> isotherm_kd;  // [?]
   std::vector<double> freundlich_n; // [?]
   std::vector<double> langmuir_b;  // [?]
-  void Print(void) const {
-    namespace util = alquimia::drivers::utilities;
-    std::cout << "  -- Material Properties :" << std::endl;
-    std::cout << "    volume : " << this->volume << std::endl;
-    util::PrintVector("    isotherm_kd", this->isotherm_kd);
-    util::PrintVector("    freundlich_n", this->freundlich_n);
-    util::PrintVector("    langmuir_b", this->langmuir_b);
-    std::cout << std::endl;
-  };
+  void Print(void) const;
 };
 
 struct DemoGeochemicalConstraint {
@@ -85,18 +57,15 @@ struct DemoGeochemicalConstraint {
   std::string constraint_type;
   std::string associated_species;
   double value;
-  void Print(void) const {
-    std::cout << "        " << this->primary_species << std::endl;
-    std::cout << "            type : " << this->constraint_type << std::endl;
-    std::cout << "            associated : " << this->associated_species << std::endl;
-    std::cout << "            value : " << this->value << std::endl;
-  };
+  void Print(void) const;
 };
   
 typedef std::vector<DemoGeochemicalConstraint> DemoGeochemicalCondition;
 
 typedef std::map<std::string, DemoGeochemicalCondition> DemoConditions;
 
+void PrintGeochemicalConditions(
+    const alquimia::drivers::utilities::DemoConditions& conditions);
 
 }  // namespace utilities
 }  // namespace drivers
