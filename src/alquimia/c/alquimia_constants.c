e1ac6ff889bc9d4ab42c9576e52f79641f8b2b3b
Ben Andre <Ben Andre@localhost>
2013-01-22 17:45:11 -0800

    use extern for c global constants to get rid of unused variable warnings

---
+++ src/alquimia/c/alquimia_constants.c
@@ -0,0 +1,19 @@
+/* -*-  mode: c++; c-default-style: "google"; indent-tabs-mode: nil -*- */
+
+#include "alquimia_constants.h"
+
+#include "stddef.h"
+
+/* String lengths */
+const size_t ALQUIMIA_MAX_STRING_LENGTH = 512;
+const size_t ALQUIMIA_MAX_WORD_LENGTH = 32;
+
+/* Geochemistry Engine Strings */
+const char* kPFloTran = "PFloTran";
+const char* kCrunchFlow = "CrunchFlow";
+
+/* Error Codes */
+const int ALQUIMIA_NO_ERROR = 0;
+const int ALQUIMIA_ERROR_INVALID_ENGINE = 1;
+const int ALQUIMIA_ERROR_ENGINE_INTEGRITY = 4577;
+
