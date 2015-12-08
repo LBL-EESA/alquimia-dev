8eb35d6ee9334c784ed250441e490ba2586fa804
Jeffrey Johnson <jnjohnson@lbl.gov>
2015-12-07 21:58:12 -0800

    Benchmark cleanup.
    * Moved batch chemistry benchmarks into benchmarks/batch_chem dir.
    * Added prefix to benchmarks to indicate what kind they are.

--- cmake/Modules/add_alquimia_batch_chem_benchmark.cmake
+++ cmake/Modules/add_alquimia_batch_chem_benchmark.cmake
@@ -1,6 +1,6 @@
 # This function adds a batch chemistry benchmark test for Alquimia.
 function(add_alquimia_batch_chem_benchmark benchmark input_file)
   set(exe ${PROJECT_BINARY_DIR}/drivers/batch_chem)
-  add_test(${benchmark} ${exe} -d -i ${input_file})
-  set_tests_properties(${benchmark} PROPERTIES WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+  add_test(batch_chem_${benchmark} ${exe} -d -i ${input_file})
+  set_tests_properties(batch_chem_${benchmark} PROPERTIES WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 endfunction()
