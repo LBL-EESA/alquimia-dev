6718dd72c6159ed97df0d584acd3589eb7758946
Glenn Hammond <gehammo@sandia.gov>
2017-02-13 11:29:55 -0800

    Added missing unzip

--- .install-linux-deps.sh
+++ .install-linux-deps.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y cmake gcc libopenmpi-dev openmpi-bin liblapack-dev gfort
 
 # Go get PETSc and build it.
 wget https://bitbucket.org/petsc/petsc.zip
+unzip -q petsc.zip
 pushd $PETSC_DIR
 ./configure --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1 --download-pflotran
 make
