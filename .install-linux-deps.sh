97df1510ba02ca41e6f6dc0909258f307ae7a245
Glenn Hammond <gehammo@sandia.gov>
2017-02-13 11:55:28 -0800

    Fixed url

--- .install-linux-deps.sh
+++ .install-linux-deps.sh
@@ -3,7 +3,7 @@ sudo apt-get update -qq
 sudo apt-get install -y cmake gcc libopenmpi-dev openmpi-bin liblapack-dev gfortran git
 
 # Go get PETSc and build it.
-git clone https://bitbucket/org/petsc/petsc petsc
+git clone https://bitbucket.org/petsc/petsc petsc
 pushd $PETSC_DIR
 ./configure --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1 --download-pflotran
 make
