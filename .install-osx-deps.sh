de55717d497fd16992ccd108590a4817a168c90f
Sergi Molins <smolins@lbl.gov>
2018-07-12 11:35:45 -0700

    added DEBUG, uninstall oclint, use gcc/7

--- .install-osx-deps.sh
+++ .install-osx-deps.sh
@@ -1,11 +1,12 @@
 # Install required software
+brew cask uninstall oclint
 brew update
 brew install openmpi
 export TMPDIR=/tmp
 
 # Make sure the weird gfortran library links are in place.
-ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
-ln -s /usr/local/lib/gcc/5/libgfortran.a /usr/local/lib/libgfortran.a
+ln -s /usr/local/lib/gcc/7/libgfortran.dylib /usr/local/lib/libgfortran.dylib
+ln -s /usr/local/lib/gcc/7/libgfortran.a /usr/local/lib/libgfortran.a
 
 # Go get PETSc 3.6.x and build it.
 git clone https://bitbucket.org/petsc/petsc petsc
