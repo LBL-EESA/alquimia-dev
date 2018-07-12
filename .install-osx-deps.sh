482b524f5ed3b3d83cea26d107ca9dedc0e0327f
Sergi Molins <smolins@lbl.gov>
2018-07-11 18:55:17 -0700

    which mpif90

--- .install-osx-deps.sh
+++ .install-osx-deps.sh
@@ -1,10 +1,8 @@
 # Install required software
 brew update
 brew install gcc
-brew install gfortran
 brew install open-mpi
 export TMPDIR=/tmp
-which gfortran
 
 # Make sure the weird gfortran library links are in place.
 ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
@@ -13,7 +11,7 @@ ln -s /usr/local/lib/gcc/5/libgfortran.a /usr/local/lib/libgfortran.a
 # Go get PETSc 3.6.x and build it.
 git clone https://bitbucket.org/petsc/petsc petsc
 pushd $PETSC_DIR
-./configure --with-mpi=1 --with-debugging=$DEBUG --with-shared-libraries=1 --download-pflotran --with-fc=gfortran
+./configure --with-mpi=1 --with-debugging=$DEBUG --with-shared-libraries=1 --download-pflotran --with-fc=`which mpif90`
 make
 make test
 popd
