52fed34a675894d71ed899a3ecf105f15670d87b
Sergi Molins <smolins@lbl.gov>
2018-07-11 20:55:55 -0700

    is it gcc/8

--- .install-osx-deps.sh
+++ .install-osx-deps.sh
@@ -2,11 +2,10 @@
 brew update
 brew install open-mpi
 export TMPDIR=/tmp
-gfortran --version
 
 # Make sure the weird gfortran library links are in place.
-ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
-ln -s /usr/local/lib/gcc/5/libgfortran.a /usr/local/lib/libgfortran.a
+ln -s /usr/local/lib/gcc/8/libgfortran.dylib /usr/local/lib/libgfortran.dylib
+ln -s /usr/local/lib/gcc/8/libgfortran.a /usr/local/lib/libgfortran.a
 
 # Go get PETSc 3.6.x and build it.
 git clone https://bitbucket.org/petsc/petsc petsc
