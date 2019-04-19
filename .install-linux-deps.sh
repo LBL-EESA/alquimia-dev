127c096f50fe6fee3b57e6e1812aecf768899ec3
Sergi Molins <smolins@lbl.gov>
2019-04-19 16:18:46 -0700

    adds crunchtope as engine with smoke tests in Travis CI

--- .install-linux-deps.sh
+++ .install-linux-deps.sh
@@ -8,6 +8,14 @@ git clone https://bitbucket.org/petsc/petsc petsc
 pushd $PETSC_DIR
 ./configure --with-mpi=1 --download-hdf5 --with-debug=$DEBUG --with-shared-libraries=1 --download-pflotran --download-pflotran-commit=origin/master
 make
+##popd
+
+# Get and build CrunchTope, inside external packages for convenience
+cd externalpackages
+git clone https://bitbucket.org/crunchflow/crunchtope-dev.git --branch petsc-upgrade
+cd crunchtope-dev/source
+git apply MakefileForAlquimia.patch
+make libcrunchchem.a
 popd
 
 # Go get pflotran and build it.
