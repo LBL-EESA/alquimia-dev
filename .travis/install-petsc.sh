ae7b345182e32b75446a04d28bb603865ffe5b06
Sergi Molins <smolins@lbl.gov>
2020-08-11 14:05:11 -0700

    update to petsc v13.3

--- .travis/install-petsc.sh
+++ .travis/install-petsc.sh
@@ -2,6 +2,6 @@
 
 git clone https://gitlab.com/petsc/petsc.git $PETSC_DIR
 cd $PETSC_DIR
-git checkout v3.11.3
+git checkout v3.13
 ./configure --with-mpi=1 --with-debug=1 --with-shared-libraries=1 --download-fblaslapack=1 --with-debug=$DEBUG
 make
