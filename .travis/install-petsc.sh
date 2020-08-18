be998767a2d8bddf8c846ad87c6591daa8ac41a2
Sergi Molins <smolins@lbl.gov>
2020-08-18 14:12:50 -0700

    fix to petsc build that breaks crunchflow

--- .travis/install-petsc.sh
+++ .travis/install-petsc.sh
@@ -2,6 +2,6 @@
 
 git clone https://gitlab.com/petsc/petsc.git $PETSC_DIR
 cd $PETSC_DIR
-git checkout v3.13
+git checkout balay/fix-make-f90-target/maint
 ./configure --with-mpi=1 --with-debug=1 --with-shared-libraries=1 --download-fblaslapack=1 --with-debug=$DEBUG
 make
